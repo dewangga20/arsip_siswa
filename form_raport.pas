67436516cfd1d6805d5bd84e8a18f77261c8fefe
aviefansary97 <aviefansaryRR@gmail.com>
2017-12-04 18:01:51 +0800

    baru di tambahkan form

---
+++ form_raport.pas
@@ -0,0 +1,24 @@
+unit form_raport;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  Traport = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  raport: Traport;
+
+implementation
+
+{$R *.dfm}
+
+end.
